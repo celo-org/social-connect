88e0c63aeb7a0349ac0a39cab0b5823c4e8100ac
Aaron <aaron.deruvo@clabs.co>
2023-10-23 15:20:51 +0200

    run publish for real

--- publish.ts
+++ publish.ts
@@ -8,12 +8,12 @@ const FAKE_OUTPUT = `
 🦋  warn @celo/encrypted-backup is not being published because version 5.0.4 is already published on npm
 🦋  warn @celo/identity is not being published because version 5.0.4 is already published on npm
 🦋  info Publishing "@celo/phone-number-privacy-common" at "3.0.4-alpha-964a99345"
-🦋  info Publishing "@celo/identity-common" at "3.0.4-alpha-964a99345"
+🦋  info Publishing "@celo/identity" at "3.0.4-alpha-964a99345"
 `
 // publish packages as alpha and return the list of packages published
 function publish() {
-  // const snapshot = child_process.execSync('yarn cs publish --tag alpha --no-git-tag')
-  const snapshot = child_process.execSync(`echo "${FAKE_OUTPUT}"`)
+  const snapshot = child_process.execSync('yarn cs publish --tag alpha --no-git-tag')
+  // const snapshot = child_process.execSync(`echo "${FAKE_OUTPUT}"`)
   const arrayOfLines = snapshot.toString().split('\n').map((line: string) => {
     const matches = line.match(/info Publishing @celo.*$/)
     return matches
