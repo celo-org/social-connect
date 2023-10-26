f370dc66e920cd545fae02136568d01db1be55b9
Aaron <aaron.deruvo@clabs.co>
2023-10-26 12:52:12 +0200

    publish is actually just for alphas

--- publish-alpha.ts
+++ publish-alpha.ts
@@ -1,20 +1,9 @@
 import * as child_process from 'child_process'
 
-const FAKE_OUTPUT = `
-🦋  info npm info @celo/phone-number-privacy-common
-🦋  info npm info @celo/encrypted-backup
-🦋  info npm info @celo/identity
-🦋  info @celo/phone-number-privacy-common is being published because our local version (3.0.4-alpha-964a99345) has not been published on npm
-🦋  warn @celo/encrypted-backup is not being published because version 5.0.4 is already published on npm
-🦋  warn @celo/identity is not being published because version 5.0.4 is already published on npm
-🦋  info Publishing "@celo/phone-number-privacy-common" at "3.0.4-alpha-964a99345"
-🦋  info Publishing "@celo/identity" at "3.0.4-alpha-964a99345"
-`
 // publish packages as alpha and return the list of packages published
 function publish() {
   try {
     const snapshot = child_process.execSync('yarn cs publish --tag alpha --no-git-tag')
-    // const snapshot = child_process.execSync(`echo "${FAKE_OUTPUT}"`)
     const arrayOfLines = snapshot
       .toString()
       .split('\n')
@@ -32,7 +21,11 @@ function publish() {
     const result = pkgs.length ? JSON.stringify(pkgs) : 'no-op'
     process.stdout.write(result)
   } catch (err) {
-    process.stderr.write(err)
+    if (err instanceof Error) {
+      process.stderr.write(err.message)
+    } else {
+      process.stderr.write((err as string).toString())
+    }
   }
 }
 
